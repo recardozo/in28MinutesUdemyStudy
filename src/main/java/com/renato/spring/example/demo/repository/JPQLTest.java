82b7d54409e7b1b9018d1b564b0e1805814cc623
RECARDOZ <RECARDOZ@NBGSW05.corp.ad.emb>
2019-02-05 10:04:56 -0200

    Using Joins

--- src/main/java/com/renato/spring/example/demo/repository/JPQLTest.java
+++ src/main/java/com/renato/spring/example/demo/repository/JPQLTest.java
@@ -90,4 +90,37 @@ public class JPQLTest {
 		logger.info("select s from Student s where s.passport.number like '%1234%-> {}", resultList);
 	}
 
+	@Test
+	public void join() {
+		Query query = em.createQuery("Select c, s from Course c JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Results Size -> {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course{} Student{}", result[0], result[1]);
+		}
+
+	}
+
+	@Test
+	public void left_join() {
+		Query query = em.createQuery("Select c, s from Course c LEFT JOIN c.students s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Results Size -> {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course{} Student{}", result[0], result[1]);
+		}
+
+	}
+
+	@Test
+	public void cross_join() {
+		Query query = em.createQuery("Select c, s from Course c, Student s");
+		List<Object[]> resultList = query.getResultList();
+		logger.info("Result Size - {}", resultList.size());
+		for (Object[] result : resultList) {
+			logger.info("Course {} Student{}", result[0], result[1]);
+		}
+
+	}
+
 }
